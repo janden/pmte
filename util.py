1e46130b7b33e7898f5f30f5509beb8ead44cc4e
Joakim Andén <janden@kth.se>
2020-04-17 15:42:19 +0200

    Add subspace_dist to utils

--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 
+from scipy.sparse.linalg import LinearOperator, eigs
 
 def ensure_dir_exists(filename):
     dirname = os.path.dirname(filename)
@@ -24,3 +25,23 @@ def cos_max_principal_angle(X, Y):
     _, S, _ = np.linalg.svd(M)
 
     return S[-1]
+
+
+def subspace_dist(X, Y):
+    p = X.shape[1]
+
+    X = np.linalg.qr(X.T)[0].T
+    Y = np.linalg.qr(Y.T)[0].T
+
+    op = lambda v: (v @ X.T) @ X - (v @ Y.T) @ Y
+    op_transp = lambda v: op(v.T).T
+
+    v = np.random.randn(p)
+
+    linop = LinearOperator(2 * (p,), matvec=op_transp)
+
+    lam, _ = eigs(linop, k=1, which='LM')
+
+    dist = np.abs(lam[0])
+
+    return dist
