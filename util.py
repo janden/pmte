697c4a86f30a555f8ed3d176aa5bf8d7062d4a8d
Joakim Andén <janden@kth.se>
2020-04-20 16:48:06 +0200

    Simplify subspace functions in util
    Just use SciPy's implementation of subspace_angles (more accurate).

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 
-from scipy.sparse.linalg import LinearOperator, eigs
+from scipy.linalg import subspace_angles
 
 def ensure_dir_exists(filename):
     dirname = os.path.dirname(filename)
@@ -10,38 +10,12 @@ def ensure_dir_exists(filename):
 
 
 def cos_max_principal_angle(X, Y):
-    # NOTE: Doesn't work if X and Y are rank-deficient.
+    theta = np.max(subspace_angles(X.T, Y.T))
 
-    d = X.shape[0]
-
-    X = np.reshape(X, (d, -1)).T
-    Y = np.reshape(Y, (d, -1)).T
-
-    X, RX = np.linalg.qr(X, 'reduced')
-    Y, RY = np.linalg.qr(Y, 'reduced')
-
-    M = X.T @ Y
-
-    _, S, _ = np.linalg.svd(M)
-
-    return S[-1]
+    return np.cos(theta)
 
 
 def subspace_dist(X, Y):
-    p = X.shape[1]
+    theta = np.max(subspace_angles(X.T, Y.T))
 
-    X = np.linalg.qr(X.T)[0].T
-    Y = np.linalg.qr(Y.T)[0].T
-
-    op = lambda v: (v @ X.T) @ X - (v @ Y.T) @ Y
-    op_transp = lambda v: op(v.T).T
-
-    v = np.random.randn(p)
-
-    linop = LinearOperator(2 * (p,), matvec=op_transp)
-
-    lam, _ = eigs(linop, k=1, which='LM')
-
-    dist = np.abs(lam[0])
-
-    return dist
+    return np.sin(theta)
