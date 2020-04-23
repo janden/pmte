bf466aa18a531d4618cf7f27092bf2c41b3eac29
Joakim Andén <janden@kth.se>
2020-04-23 12:43:19 +0200

    Add command-line tool to diff binary floats

---
+++ diff_float32.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np
+
+import util
+
+
+def main():
+    args = sys.argv[1:]
+
+    fname1 = args[0]
+    fname2 = args[1]
+
+    X1 = util.load_float32(fname1)
+    X2 = util.load_float32(fname2)
+
+    n = np.linalg.norm(X1 - X2)
+
+    print(n)
+
+
+if __name__ == '__main__':
+    main()
