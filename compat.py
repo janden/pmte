8aa2cb7ed8ac7590809fed3bdafdf2a7592310a9
Joakim Andén <janden@kth.se>
2020-05-04 11:21:25 +0200

    Prevent oct2py from removing trailing dims

--- compat.py
+++ compat.py
@@ -5,5 +5,5 @@ import oct2py
 def oct_randn():
     oc = oct2py.Oct2Py()
     oc.randn('state', 0)
-    gen_fun = lambda sz: np.transpose(oc.randn(*sz[::-1]))
+    gen_fun = lambda sz: np.transpose(oc.randn(*sz[::-1])).reshape(sz)
     return gen_fun
