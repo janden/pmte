5f39e16516c830d438b1c537b9bb27942054b51b
Joakim Andén <janden@kth.se>
2020-06-05 10:44:09 +0200

    Bring rho1_single into the future
    Use proper taper calculation, NumPy RNG, proper definition of rho0,
    output log error slope.

--- rho1_single.py
+++ rho1_single.py
@@ -19,13 +19,24 @@ def main():
 
     err1 = []
 
+    rng = np.random.default_rng(0)
+    gen_fun = rng.standard_normal
+
+    err1 = np.empty_like(rs)
+
     for k, r in enumerate(rs):
         mask = R < (r * N)
-        h = calc_rand_tapers(mask, W)
+        h = calc_rand_tapers(mask, W, gen_fun=gen_fun, use_fftw=True,
+                use_sinc=True, p=0, b=8)
         rho = 1 / h.shape[0] * np.sum(np.abs(np.fft.fft2(h)) ** 2, axis=0)
-        rho0 = 1 / (2 * W) ** 2 * ((np.abs(fX) < W) & (np.abs(fY) < W))
 
-        err1.append(np.linalg.norm(rho.ravel() - rho0.ravel(), 1) / rho.size)
+        rho0 = np.zeros_like(rho)
+        rho0[(np.abs(fX) < W) & (np.abs(fY) < W)] = 1 / (2 * W) ** 2
+        rho0[(np.abs(fX) == W) & (np.abs(fY) < W)] = 1 / (2 * W) ** 2 / 2
+        rho0[(np.abs(fX) < W) & (np.abs(fY) == W)] = 1 / (2 * W) ** 2 / 2
+        rho0[(np.abs(fX) == W) & (np.abs(fY) == W)] = 1 / (2 * W) ** 2 / 4
+
+        err1[k] = np.linalg.norm(rho.ravel() - rho0.ravel(), 1) / rho.size
 
     fname = 'data/rho1_single.csv'
 
@@ -34,6 +45,10 @@ def main():
     with open(fname, 'w') as f:
         for r, err in zip(rs, err1):
             f.write('%.15g %.15g\n' % (r * N, err))
+
+    beta = util.log_slope(rs * N, err1)
+
+    print('Log error slope: %g' % beta)
 
 
 if __name__ == '__main__':
