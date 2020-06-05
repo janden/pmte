d4b15b3ea4b16f09d532cd0e6d0c207f50d9e907
Joakim Andén <janden@kth.se>
2020-06-06 00:04:03 +0200

    Speed up estimate_psd_tapers
    Better use of the SciPy FFT. Introduce support for FFTW.

--- estimation.py
+++ estimation.py
@@ -274,39 +274,95 @@ def estimate_psd_periodogram(x, d):
     return x_per
 
 
-def estimate_psd_rand_tapers(x, mask, W=1/8, p=5, b=3, use_sinc=False, gen_fun=None):
+def estimate_psd_rand_tapers(x, mask, W=1/8, p=5, b=3, use_sinc=False,
+        use_fftw=False, gen_fun=None):
     h = calc_rand_tapers(mask, W=W, p=p, b=b, gen_fun=gen_fun,
-            use_sinc=use_sinc, use_fftw=True)
+            use_sinc=use_sinc, use_fftw=use_fftw)
 
-    x_rt = estimate_psd_tapers(x, h)
+    x_rt = estimate_psd_tapers(x, h, use_fftw=use_fftw)
 
     return x_rt
 
-def estimate_psd_multitaper(x, d, W=1/8):
+def estimate_psd_multitaper(x, d, W=1/8, use_fftw=False):
     shape = x.shape[-d:]
 
     h = calc_tensor_tapers(shape, W=W)
 
-    x_mt = estimate_psd_tapers(x, h)
+    x_mt = estimate_psd_tapers(x, h, use_fftw=use_fftw)
 
     return x_mt
 
 
-def estimate_psd_tapers(x, tapers):
+def estimate_psd_tapers(x, tapers, use_fftw=False):
     d = tapers.ndim - 1
+
+    if d > 2 and use_fftw:
+        raise RuntimeError('FFTW is not supported for d > 2')
 
     sig_sz = tapers.shape[-d:]
     taper_len = tapers.shape[0]
 
-    real_dtype = np.real(x.ravel()[0]).dtype
-    x_mt = np.zeros_like(x, dtype=real_dtype)
+    precision = np.real(x.ravel()[0]).dtype.itemsize
 
-    for taper in tapers:
-        x_tapered = x * taper
+    real_dtype = {4: 'float32', 8: 'float64'}[precision]
+    complex_dtype = {4: 'complex64', 8: 'complex128'}[precision]
 
-        x_tapered_f = fftn(x_tapered, axes=range(-d, 0), workers=-1)
+    is_real = (np.real(x.ravel()[0]).dtype == x.dtype)
 
-        x_mt += x_tapered_f.real ** 2 + x_tapered_f.imag ** 2
+    if use_fftw:
+        n_threads = 8
+
+        if is_real:
+            in_array = pyfftw.empty_aligned(x.shape,
+                                            dtype=real_dtype)
+            out_array = pyfftw.empty_aligned(x.shape[:-1]
+                                            + (x.shape[-1] // 2 + 1,),
+                                            dtype=complex_dtype)
+        else:
+            in_array = pyfftw.empty_aligned(x.shape,
+                                            dtype=complex_dtype)
+            out_array = pyfftw.empty_aligned(x.shape,
+                                             dtype=complex_dtype)
+
+        x_mt = np.zeros(out_array.shape, dtype=real_dtype)
+
+        plan = pyfftw.FFTW(in_array, out_array, axes=range(-d, 0),
+                           direction='FFTW_FORWARD',
+                           flags=('FFTW_MEASURE',),
+                           threads=n_threads)
+
+        for taper in tapers:
+            np.multiply(x, taper, out=in_array)
+
+            plan()
+
+            np.add(x_mt, out_array.real ** 2 + out_array.imag ** 2, out=x_mt)
+
+        if is_real:
+            # NOTE: The below only works for d = 1 and d = 2. For higher
+            # dimensions, we need to handle the Nyquist frequency separately
+            # (a Fourier, or Hermitian, flip).
+
+            # Otherwise the indexing below will destroy the original x_mt.
+            x_mt_flip = x_mt[..., -2:0:-1].copy()
+
+            # Don't want to flip the first (i.e., Nyquist) frequency.
+            ixgrid = np.ix_(*(range(sz) for sz in x_mt_flip.shape[:-d]), *(range(1,
+                sz) for sz in x_mt_flip.shape[-d:-1]), range(x_mt_flip.shape[-1]))
+
+            x_mt_flip[ixgrid] = np.flip(x_mt_flip[ixgrid], axis=range(-d, -1))
+
+            x_mt = np.concatenate((x_mt, x_mt_flip), axis=-1)
+    else:
+        x_mt = np.zeros_like(x, dtype=real_dtype)
+        x_tapered = np.empty_like(x)
+
+        for taper in tapers:
+            np.multiply(x, taper, out=x_tapered)
+
+            x_tapered_f = fftn(x_tapered, axes=range(-d, 0), workers=-1)
+
+            np.add(x_mt, x_tapered_f.real ** 2 + x_tapered_f.imag ** 2, out=x_mt)
 
     x_mt /= taper_len
 
