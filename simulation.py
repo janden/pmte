1cb84373b54db7257aa6446ccb840349786c0b8a
Joakim Andén <janden@kth.se>
2020-06-16 12:08:45 +0200

    Allow complex fields in generate_field

--- simulation.py
+++ simulation.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def generate_field(sig_sz, n, psd_fun=None, gen_sig_sz=None, gen_fun=None):
+def generate_field(sig_sz, n, psd_fun=None, gen_sig_sz=None, gen_fun=None,
+        real=True):
     if psd_fun is None:
         psd_fun = lambda x, y: np.ones_like(x)
 
@@ -21,18 +22,32 @@ def generate_field(sig_sz, n, psd_fun=None, gen_sig_sz=None, gen_fun=None):
 
     filter_f = np.sqrt(np.maximum(0, psd_fun(*grids)))
 
-    x = np.zeros((n,) + sig_sz)
+    if real:
+        dtype = 'float64'
+    else:
+        dtype = 'complex128'
+
+    x = np.zeros((n,) + sig_sz, dtype=dtype)
 
     block_count = int(np.ceil(n / block_size))
 
     for ell in range(block_count):
         n_block = min(n - ell * block_size, block_size)
 
-        w = gen_fun((n_block,) + gen_sig_sz)
+        gen_block_sz = (n_block,) + gen_sig_sz
+
+        if real:
+            w = gen_fun(gen_block_sz)
+        else:
+            w = 1 / np.sqrt(2) * (gen_fun(gen_block_sz)
+                                  + 1J * gen_fun(gen_block_sz))
 
         wf = np.fft.fftn(w, axes=range(-d, 0))
         xf_block = wf * filter_f
-        x_block = np.real(np.fft.ifftn(xf_block, axes=range(-d, 0)))
+        x_block = np.fft.ifftn(xf_block, axes=range(-d, 0))
+
+        if real:
+            x_block = np.real(x_block)
 
         ixgrid = np.ix_(range(n_block), *(range(sz) for sz in sig_sz))
 
