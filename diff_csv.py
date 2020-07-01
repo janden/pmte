5e35503b72042db28c4059b685c0d5f5ec6401e2
Joakim Andén <janden@kth.se>
2020-07-01 21:00:57 +0200

    Catch errors in diff scripts

--- diff_csv.py
+++ diff_csv.py
@@ -8,8 +8,12 @@ def main():
     fname1 = args[0]
     fname2 = args[1]
 
-    X1 = np.loadtxt(fname1)
-    X2 = np.loadtxt(fname2)
+    try:
+        X1 = np.loadtxt(fname1)
+        X2 = np.loadtxt(fname2)
+    except OSError:
+        print('not found')
+        return
 
     n = np.linalg.norm(X1 - X2)
 
--- diff_float32.py
+++ diff_float32.py
@@ -10,8 +10,12 @@ def main():
     fname1 = args[0]
     fname2 = args[1]
 
-    X1 = util.load_float32(fname1)
-    X2 = util.load_float32(fname2)
+    try:
+        X1 = util.load_float32(fname1)
+        X2 = util.load_float32(fname2)
+    except FileNotFoundError:
+        print('not found')
+        return
 
     n = np.linalg.norm(X1 - X2)
 
