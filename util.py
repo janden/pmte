f9a7893e6bfacadc49cae17187a9ccd2e2ee654d
Joakim Andén <janden@kth.se>
2020-09-17 09:13:18 +0200

    Add normalization and shifting to util.grid

--- util.py
+++ util.py
@@ -170,13 +170,19 @@ def log_slope(x, y):
     return beta
 
 
-def grid(sz):
+def grid(sz, normalized=False, shifted=True):
     sz = np.array(sz)
 
     if sz.ndim == 0:
         sz = sz[np.newaxis]
 
     rngs = [np.ceil(np.arange(-N / 2, N / 2)) for N in sz]
+
+    if normalized:
+        rngs = [rng / N for rng, N in zip(rngs, sz)]
+
+    if not shifted:
+        rngs = [np.fft.ifftshift(rng) for rng in rngs]
 
     grid = np.meshgrid(*rngs, indexing='ij')
 
