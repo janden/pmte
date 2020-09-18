93f23c8ecc6086af5347baee0f761044b92ca9a6
Joakim Andén <janden@kth.se>
2020-09-18 16:04:42 +0200

    Add save_json to datahelpers

--- scripts/datahelpers.py
+++ scripts/datahelpers.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import json
 
 from scipy.ndimage import rotate
 
@@ -97,3 +98,13 @@ def save_image(name, image):
     ensure_dir_exists(filename)
 
     write_gplt_binary_matrix(filename, image)
+
+
+def save_json(name, values):
+    filename = os.path.join(_root_dir(), 'data', name + '.json')
+
+    ensure_dir_exists(filename)
+
+    with open(filename, 'w') as f:
+        json.dump(values, f)
+        f.write('\n')
