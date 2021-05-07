bc6bf80018b3a48b8b4459253d9e3e810f063755
Joakim Andén <janden@kth.se>
2021-05-07 07:21:49 -0500

    Move functions from util to helpers
    The functions are: `log_slope`, `target_spectral_window`, `disk_mask`,
    and `square_mask`. These aren't strictly needed to generate or use the
    tapers, so we move them out of the core code.

--- src/pmte/util.py
+++ src/pmte/util.py
@@ -1,18 +1,4 @@
-import os
-
 import numpy as np
-
-
-def log_slope(x, y):
-    x = np.log(x)
-    y = np.log(y)
-
-    x = x - np.mean(x)
-    y = y - np.mean(y)
-
-    beta = np.dot(x, y) / np.dot(x, x)
-
-    return beta
 
 
 def grid(sz, normalized=True, shifted=False):
@@ -32,36 +18,3 @@ def grid(sz, normalized=True, shifted=False):
     grid = np.meshgrid(*rngs, indexing='ij')
 
     return grid
-
-
-def target_spectral_window(Nf, W, shifted=False):
-    fX, fY = grid((Nf, Nf), shifted=shifted)
-
-    rho = np.zeros((Nf, Nf))
-
-    half_W = W / 2
-
-    rho[(np.abs(fX) < half_W) & (np.abs(fY) < half_W)] = 1 / W ** 2
-    rho[(np.abs(fX) == half_W) & (np.abs(fY) < half_W)] = 1 / W ** 2 / 2
-    rho[(np.abs(fX) < half_W) & (np.abs(fY) == half_W)] = 1 / W ** 2 / 2
-    rho[(np.abs(fX) == half_W) & (np.abs(fY) == half_W)] = 1 / W ** 2 / 4
-
-    return rho
-
-
-def disk_mask(N, R):
-    x1, x2 = grid((N, N), normalized=False, shifted=True)
-
-    r = np.hypot(x1, x2)
-
-    mask = (r < R)
-
-    return mask
-
-
-def square_mask(N, R):
-    x1, x2 = grid((N, N), normalized=False, shifted=True)
-
-    mask = (-R <= x1) & (x1 < R) & (-R <= x2) & (x2 < R)
-
-    return mask
